16cce4dc0170a6544ad4b311ff653ad13eaa396f
MohsenNiazmand <it.mohsen.niazmand@gmail.com>
2024-03-27 03:51:35 -0700

    second lesson

--- ContactsList/index.js
+++ ContactsList/index.js
@@ -7,20 +7,52 @@ const contactList = [];
 
 console.log('--- Contacts List ---');
 
-const firstName=await rl.question('First Name : ');
-const lastName=await rl.question('Last Name : ');
 
-const newContact={
-    id:contactList.length,
-    firstName,
-    lastName
-};
 
-contactList.push(newContact);
+async function addNewContact() {
+    const firstName = await rl.question('First Name : ');
+    const lastName = await rl.question('Last Name : ');
 
-const formattedContactList=contactList.map(({id,firstName,lastName})=> '# '+id+' '+firstName+' '+lastName).join('\n');
+    const newContact = {
+        id: contactList.length,
+        firstName,
+        lastName
+    };
 
-console.log('Contacts List: ');
-console.log(formattedContactList);
+    contactList.push(newContact);
+}
 
-rl.close();
+
+
+
+function showContactsList() {
+    const formattedContactList = contactList.map(({ id, firstName, lastName }) => '# ' + id + ' ' + firstName + ' ' + lastName).join('\n');
+
+    console.log('Contacts List: ');
+    console.log(formattedContactList);
+}
+
+
+function quit() {
+    rl.close();
+}
+
+
+async function help() {
+    console.log('n: Add new contact\nl: show contacts list\nq: quit');
+    const action = await rl.question('Enter your action:');
+    if (action == 'n') {
+        await addNewContact();
+    } else if (action === 'l') {
+        showContactsList();
+    } else {
+        quit();
+        return;
+    }
+    help();
+}
+
+await help();
+// await addNewContact();
+// showContactsList();
+// quit();
