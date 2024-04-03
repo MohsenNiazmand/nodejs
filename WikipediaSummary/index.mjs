f8101b66b7899e666b9dd3f4025b247c2bd1184d
MohsenNiazmand <it.mohsen.niazmand@gmail.com>
2024-04-03 05:21:16 -0700

    last lesson HTTP

--- WikipediaSummary/index.mjs
+++ WikipediaSummary/index.mjs
@@ -1,11 +1,30 @@
+import express from "express";
 import {load} from "cheerio";
 
-const response =await fetch('https://en.wikipedia.org/wiki/Node.js');
-const data = await response.text();
+const app=express();
 
-const $ = load(data)
+app.get('/topic/:topic',async (req,res)=>{
+
+    const response =await fetch(`https://en.wikipedia.org/wiki/${req.params.topic}`);
+    const data = await response.text();
+    
+    const $ = load(data)
+    
+    
+    const summary = $('#mw-content-text > div.mw-parser-output > table.infobox + p').text();
+
+    if(summary.length<1){
+        res.status(404).send(`Topic ${req.params.topic} not Found!`);
+        return;
+    }
+    
+    // console.log('Wiki summary : ',summary);
+    res.send(summary);
+
+});
 
 
-const summary = $('#mw-content-text > div.mw-parser-output > table + p:first-of-type').text();
 
-console.log('Wiki summary : ',summary);
+app.listen(3000,()=>{
+    console.log('Wikipedia Summary is running on port 3000');
+});
