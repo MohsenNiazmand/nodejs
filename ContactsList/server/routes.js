2db748735ed2763bd0198bf6927d82c373f24e1f
MohsenNiazmand <it.mohsen.niazmand@gmail.com>
2024-04-03 02:33:16 -0700

    delete contact http

--- ContactsList/server/routes.js
+++ ContactsList/server/routes.js
@@ -1,26 +1,26 @@
-import  express  from "express";
-import { formatContactsList,generateNewContactId,loadContacts, saveContacts } from '../services.js';
+import express from "express";
+import { formatContactsList, generateNewContactId, loadContacts, saveContacts } from '../services.js';
 
 const contactList = [];
 
 const router = express.Router();
 
-router.get('/list',(req,res)=>{
-    if(req.query.format){
-        const responseData=`<pre>${formatContactsList(contactList)}</pre>`;
+router.get('/list', (req, res) => {
+    if (req.query.format) {
+        const responseData = `<pre>${formatContactsList(contactList)}</pre>`;
         res.type('html');
         res.send(responseData);
         return;
     }
-        res.json(contactList);
-    
+    res.json(contactList);
+
 });
 
 
-router.post('/new',(req,res)=>{
-    const {firstName,lastName} = req.body;
+router.post('/new', (req, res) => {
+    const { firstName, lastName } = req.body;
 
-   const id = generateNewContactId(contactList);
+    const id = generateNewContactId(contactList);
 
     const newContact = {
         id,
@@ -36,7 +36,30 @@ router.post('/new',(req,res)=>{
 });
 
 
+router.delete('/:id', (req, res) => {
+    if (contactList.length < 1) {
+        res.status(400).send({
+            message: 'There is no contact on the list',
+        });
 
+        return;
+    }
+
+    const contactIndex = contactList.findIndex(({ id }) => id === Number(req.params.id));
+
+    if (contactIndex < 0) {
+        res.status(400).send({
+            message: 'Invalid ID',
+        });
+
+        return;
+    }
+
+    contactList.splice(contactIndex, 1);
+    saveContacts(contactList);
+
+    res.send(`Contact #${req.params.id} has been deleted!`);
+});
 const loadedContacts = await loadContacts();
 contactList.push(
     ...loadedContacts,
