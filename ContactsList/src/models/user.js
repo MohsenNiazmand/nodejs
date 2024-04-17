f52cb7e4b33d88ec64182a450e147f95af29d91b
MohsenNiazmand <it.mohsen.niazmand@gmail.com>
2024-04-17 02:01:14 -0700

    password hashing done

--- ContactsList/src/models/user.js
+++ ContactsList/src/models/user.js
@@ -1,20 +1,38 @@
 import { DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
 
-export default function(sequelize) {
-    return sequelize.define('User', {
-        fullname: {
-            type: DataTypes.STRING(40),
-            allowNull: false,
-        },
-        username: {
-          type: DataTypes.STRING(25),
-          allowNull: false,
-          unique: true,
-        },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
+
+async function hashPassword(user) {
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+}
+
+export default function (sequelize) {
+  return sequelize.define('User', {
+    fullname: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING(25),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+    {
+      hooks: {
+        beforeCreate: hashPassword,
+        beforeUpdate: hashPassword,
       },
-    );
+      istanceMethods : {
+        isValidPassword(password){
+          return bcrypt.compareSync(password,this.password);
+        }
+      }
+    }
+  );
 }
