b67fb2fb69cc9c06cf7a285ba5872c16c4d30762
MohsenNiazmand <it.mohsen.niazmand@gmail.com>
2024-04-22 00:51:43 -0700

    payments done

---
+++ MicroShop/services/Payment/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+import sql from './db.js';
+
+const STATUS = {
+    PENDING: 0,
+    PAID: 1,
+    FAILED: 2,
+};
+
+await sql`CREATE TABLE IF NOT EXISTS payment (id SERIAL, user_id INTEGER, amount INTEGER, status INTEGER)`;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/payment', async (req, res) => {
+    const payment = await sql`SELECT * FROM payment`;
+
+    res.json(payment);
+});
+
+app.post('/payment', async (req, res) => {
+    const { user_id, amount } = req.body;
+
+    const payment = await sql`INSERT INTO payment(user_id, amount, status) VALUES(${user_id}, ${amount}, ${STATUS.PENDING}) RETURNING *`;
+
+    res.json(payment);
+});
+
+app.patch('/payment/:id', async (req, res) => {
+    const { status } = req.body;
+    const payment = {
+        id: req.params.id,
+        status,
+    };
+
+    await sql`UPDATE payment set ${sql(payment, 'status')} WHERE id = ${payment.id}`;
+
+    res.send(`Payment #${payment.id} has been updated!`);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Payment service is running on port', process.env.PORT);
+});
