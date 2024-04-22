77c9bfb455bb7c05dee049a2771c6f76a8913c90
MohsenNiazmand <it.mohsen.niazmand@gmail.com>
2024-04-22 00:41:03 -0700

    second micro services

---
+++ MicroShop/services/Customers/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+import sql from './db.js';
+
+await sql`CREATE TABLE IF NOT EXISTS customers (id SERIAL, fullname TEXT, address TEXT)`;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/customers', async (req, res) => {
+    const customers = await sql`SELECT * FROM customers`;
+
+    res.json(customers);
+});
+
+app.post('/customer', async (req, res) => {
+    const { fullname, address } = req.body;
+
+    const [customer] = await sql`INSERT INTO customers(fullname, address) VALUES(${fullname}, ${address}) RETURNING *`;
+
+    res.json(customer);
+});
+
+app.delete('/customer/:id', async (req, res) => {
+    await sql`DELETE FROM customers WHERE id = ${req.params.id}`;
+
+    res.send(`Customer #${req.params.id} has been deleted!`);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Customers service is running on port', process.env.PORT);
+});
